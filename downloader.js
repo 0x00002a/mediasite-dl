739c02ddd3e23bd3323a64e3e34313139e9a77db
Ash England-Elbro <ashenglandelbro@protonmail.com>
2021-10-09 15:28:11 +0100

    chore: url extraction works

--- downloader.js
+++ downloader.js
@@ -26,9 +26,69 @@ function findVideoEl() {
   return candidates;
 }
 
-function doDownload() {
-  const vid_url = findVideoEl().getAttribute("src").replace("blob:", "");
+function getCookie(name) {
+  let decoded = decodeURIComponent(document.cookie);
+  let all_cookies = decoded.split(";");
+  let seperated = all_cookies.split(" = ");
+  let target_cookie = decoded.split(" = ").findIndex(name);
+  if (target_cookie === -1) {
+    return null;
+  } else {
+    return decoded[target_cookie + 1];
+  }
+}
+
+function calcVideoId() {
+  return document.URL.split("/").pop().split("?")[0];
+}
+
+function loadAuthCookies() {
+  const global_auth_name = "MediasiteAuth"; // If signed in using AAD (microsoft stuffs)
+  const vid_id = calcVideoId(); // If signed in using blackboard
+  const per_vid_cookie_name = "MediasiteAuthTickets-" + vid_id;
+  let per_vid_cookie = getCookie(per_vid_cookie_name);
+  let global_auth_cookie = getCookie(global_auth_name);
+  if (per_vid_cookie === null && global_auth_cookie === null) {
+    console.error("failed to find authentication cookie");
+  } else if (per_vid_cookie === null) {
+    return { global_auth_name: global_auth_cookie };
+  } else {
+    return { per_vid_cookie_name: per_vid_cookie }; // TODO: Check if either have expired and/or which is older
+  }
+}
+
+function buildVidRequestJson() {
+  return {
+    getPlayerOptionsRequest: {
+      ResourceId: calcVideoId(),
+      QueryString: "?autostart=true",
+      UseScreenReader: false,
+      UrlReferrer: document.URL,
+    },
+  };
+}
+
+function handleVidAPIResp(resp) {
+  const vid_url = resp.d.Presentation.Streams[0].VideoUrls[1].Location;
+
   console.debug("url: " + vid_url);
+  window.open(vid_url, "_blank");
+}
+
+const API_TARGET =
+  "https://mediasite.bris.ac.uk/Mediasite/PlayerService/PlayerService.svc/json/GetPlayerOptions";
+
+function doDownload() {
+  const req = new XMLHttpRequest();
+  req.open("POST", API_TARGET, true);
+  req.onload = function () {
+    console.log(this.responseText);
+    const resp = JSON.parse(this.responseText);
+    handleVidAPIResp(resp);
+  };
+  req.setRequestHeader("Content-Type", "application/json");
+  req.send(JSON.stringify(buildVidRequestJson()));
+  //console.debug("url: " + vid_url);
 
   /*const el = document.createElement("a");
   el.target = "_blank";
@@ -36,7 +96,7 @@ function doDownload() {
   document.body.appendChild(el);
   el.click();
   document.body.removeChild(el);*/
-  window.open(vid_url, "_blank");
+  //window.open(vid_url, "_blank");
 }
 
 const dl_btn_observer = new window.MutationObserver((m, o) =>
